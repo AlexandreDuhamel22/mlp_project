3d9654ccef7a6d7ad51197dd0355bcc06b9d893f
Alexandre Duhamel <147648429+AlexandreDuhamel22@users.noreply.github.com>
2025-05-12 00:24:08 -0400

    Delete ml-attacks-project directory

--- ml-attacks-project/src/utils/metrics.py
+++
@@ -1,7 +0,0 @@
-def accuracy(y_true, y_pred):
-    correct = (y_true == y_pred).sum()
-    total = y_true.shape[0]
-    return correct / total
-
-def loss(y_true, y_pred):
-    return ((y_true - y_pred) ** 2).mean()  # Mean Squared Error as an example
